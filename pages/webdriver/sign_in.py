d2d0660c01d9236acd13546ae5fc33f8c9bc0ab5
Zac <zac@zac-Ubuntu.(none)>
2012-06-29 15:48:23 +0100

    Add 'add new email' for multiple-email logins.

--- pages/webdriver/sign_in.py
+++ pages/webdriver/sign_in.py
@@ -9,11 +9,13 @@ from base import Base
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+import time
 
 class SignIn(Base):
 
-    _signed_in_email_locator = (By.CSS_SELECTOR, 'label[for=email_0]')
     _this_is_not_me_locator = (By.ID, 'thisIsNotMe')
+    _signed_in_email_locator = (By.XPATH, "//label[input[@checked='checked']]")
+    _listed_emails_locator = (By.XPATH, "//label[input[@name='email']]")
     _email_locator = (By.ID, 'email')
     _password_locator = (By.ID, 'password')
     _verify_password_locator = (By.ID, 'vpassword')
@@ -24,6 +26,9 @@ class SignIn(Base):
     _forgot_password_locator = (By.ID, 'forgotPassword')
     _reset_password_locator = (By.ID, 'password_reset')
     _check_email_at_locator = (By.CSS_SELECTOR, '#wait .contents h2 + p strong')
+    _add_another_email_locator = (By.ID, 'useNewEmail')
+    _new_email_locator = (By.ID, 'newEmail')
+    _add_new_email_locator = (By.ID, 'addNewEmail')
 
     def __init__(self, selenium, timeout, expect='new'):
         Base.__init__(self, selenium, timeout)
@@ -44,6 +49,7 @@ class SignIn(Base):
             WebDriverWait(self.selenium, self.timeout).until(
                 lambda s: s.find_element(
                     *self._sign_in_returning_user_locator).is_displayed())
+            time.sleep(2)
         else:
             raise Exception('Unknown expect value: %s' % expect)
 
@@ -62,6 +68,11 @@ class SignIn(Base):
             lambda s: s.find_element(*self._email_locator).is_displayed())
 
     @property
+    def emails(self):
+        """ Lists the emails for the returning user """
+        return [element.text for element in self.selenium.find_elements(*self._listed_emails_locator)]
+
+    @property
     def email(self):
         """Get the value of the email field."""
         return self.selenium.find_element(*self._email_locator).text
@@ -72,6 +83,31 @@ class SignIn(Base):
         email = self.selenium.find_element(*self._email_locator)
         email.clear()
         email.send_keys(value)
+
+    @property
+    def new_email(self):
+        """Get the value of the new email field."""
+        return self.selenium.find_element(*self._new_email_locator).text
+
+    @new_email.setter
+    def new_email(self, value):
+        """Set the value of the new email field."""
+        email = self.selenium.find_element(*self._new_email_locator)
+        email.clear()
+        email.send_keys(value)
+
+    @property
+    def selected_email(self):
+        """Return the value of the selected email of returning user's multiple emails"""
+        return self.signed_in_email
+
+    def select_email_checkbox(self, value):
+        """ Select email from the returning user's multiple emails """
+        checkbox = self.selenium.find_element(By.CSS_SELECTOR, "input[value='%s']" % value)
+        checkbox.click()
+
+    def check_email_at_address(self):
+        return self.selenium.find_element(*self._check_email_at_locator).text
 
     @property
     def password(self):
@@ -148,6 +184,20 @@ class SignIn(Base):
             lambda s: s.find_element(
                 *self._check_email_at_locator).is_displayed())
 
+    def click_add_another_email_address(self):
+        """Clicks 'add another email' button."""
+        self.selenium.find_element(*self._add_another_email_locator).click()
+        WebDriverWait(self.selenium, self.timeout).until(
+            lambda s: s.find_element(
+                *self._add_new_email_locator).is_displayed())
+
+    def click_add_new_email(self):
+        """Clicks 'Add' button to insert new email address."""
+        self.selenium.find_element(*self._add_new_email_locator).click()
+        WebDriverWait(self.selenium, self.timeout).until(
+            lambda s: s.find_element(
+                *self._check_email_at_locator).is_displayed())
+
     def sign_in(self, email, password):
         """Signs in using the specified email address and password."""
         self.email = email
@@ -168,3 +218,10 @@ class SignIn(Base):
     def sign_in_returning_user(self):
         """Signs in with the stored user."""
         self.click_sign_in_returning_user()
+
+    def sign_in_add_another_email(self, email):
+        self.click_add_another_email_address()
+        self.new_email = email
+        self.click_add_new_email()
+        self.close_window()
+        self.switch_to_main_window()
