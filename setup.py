1225de2e36215b0a2134f228589c7e0f699141ee
Dave Hunt <dave.hunt@gmail.com>
2016-05-27 15:01:46 +0100

    Bump version number to 2.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bidpom',
-      version='2.0',
+      version='2.0.1',
       description='Mozilla BrowserID (Persona) Page Object Model',
       long_description=open('README.rst').read(),
       author='Mozilla Web QA',
