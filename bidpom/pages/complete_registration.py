b3fcea8b6014f0b352d611e774585c023536a441
Dave Hunt <dave.hunt@gmail.com>
2016-05-27 14:52:27 +0100

    Migrate to PyPOM

--- bidpom/pages/complete_registration.py
+++ bidpom/pages/complete_registration.py
@@ -5,7 +5,6 @@
 from base import Base
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 class CompleteRegistration(Base):
@@ -16,41 +15,40 @@ class CompleteRegistration(Base):
     _user_loggedin_locator = (By.CSS_SELECTOR, '#loggedin span')
 
     def __init__(self, selenium, timeout=10, expect='success'):
-        Base.__init__(self, selenium, timeout)
+        super(CompleteRegistration, self).__init__(selenium, timeout)
 
         if expect == 'success':
-            WebDriverWait(self.selenium, self.timeout).until(
-                lambda s: s.find_element(*self._user_loggedin_locator).is_displayed())
+            self.wait.until(lambda s: self.is_element_displayed(
+                *self._user_loggedin_locator))
         elif expect == 'verify':
-            WebDriverWait(self.selenium, self.timeout).until(
-                lambda s: s.find_element(*self._password_locator).is_displayed())
+            self.wait.until(lambda s: self.is_element_displayed(
+                *self._password_locator))
         else:
             raise Exception('Unknown expect value: %s' % expect)
 
     @property
     def email(self):
         """Get the value of the email field."""
-        return self.selenium.find_element(*self._email_locator).text
+        return self.find_element(*self._email_locator).text
 
     @property
     def password(self):
         """Get the value of the password field."""
-        return self.selenium.find_element(*self._password_locator).text
+        return self.find_element(*self._password_locator).text
 
     @password.setter
     def password(self, value):
         """Set the value of the password field."""
-        password = self.selenium.find_element(*self._password_locator)
+        password = self.find_element(*self._password_locator)
         password.clear()
         password.send_keys(value)
 
     def click_finish(self):
         """Clicks the 'finish' button."""
-        self.selenium.find_element(*self._finish_locator).click()
-        WebDriverWait(self.selenium, self.timeout).until(
-            lambda s: s.find_element(*self._thank_you_locator).is_displayed())
+        self.find_element(*self._finish_locator).click()
+        self.wait.until(lambda s: self.is_element_displayed(*self._thank_you_locator))
 
     @property
     def user_loggedin(self):
         """Returns the 'thank you' message."""
-        return self.selenium.find_element(*self._user_loggedin_locator).text
+        return self.find_element(*self._user_loggedin_locator).text
